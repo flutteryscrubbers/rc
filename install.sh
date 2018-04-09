a384ab1fc881fa26b5ce21c6752f64bc00de8e21
Nia Watts <niawatts95@gmail.com>
2018-04-09 14:11:04 -0600

    general updates

--- install.sh
+++ install.sh
@@ -3,3 +3,11 @@
 # Base16 Shell
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 
+# Install NeoVim
+brew install neovim
+
+# Copy NVim Config
+cp ./init.vim ~/.config/nvim/
+
+# Install Python Dependency
+pip3 install neovim
